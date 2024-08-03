96292d0a23d1f45630f72855f504c31d18b8a130
Matheus Pinheiro <matheusedulimapinheiro@gmail.com>
2024-08-03 10:20:04 -0300

    Melhorando a rota da home para a tela do forms

--- controllers/home.py
+++ controllers/home.py
@@ -10,3 +10,8 @@ def root():
 @home_route.route('/home')
 def home():
     return render_template('index.html')
+
+
+@home_route.route('/gateway_program')
+def gateway_program():
+    return render_template('main.html')
